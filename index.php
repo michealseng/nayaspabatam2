d2a120dde61805a7d317c110ac32e0d5c475f570
Micheal Seng <michealseng@crosshatch.tv>
2019-01-06 11:36:57 -0600

    Added the php structure

---
+++ index.php
@@ -0,0 +1,176 @@
+<?php  echo file_get_contents("html/header.html"); ?>
+<?php  echo file_get_contents("html/top-nav.html"); ?>
+
+<div class="hero">
+  <div class="overlay">
+    <div class="overlay-content">
+      <h1>Most relaxing and healthy spa in Batam</h1>
+    </div>
+  </div>
+</div>
+<section class="section giveback">
+  <div class="readmode">
+    <div class="section-title">
+      <h3>Giving back to the community</h3>
+    </div>
+    <p>We are commited to give back 5% of our revenue back to the community, specifically for children. Also we are committed to help the elderly community through hydrotherapy in hope that it will help them.</p>
+  </div>
+</section>
+
+<section class="section benefits">
+  <div class="section-title">
+    <h3>Benefits of Hydrotherapy</h3>
+  </div>
+  <div class="cards grid-1120">
+    <div class="card-container">
+      <div class="card shadowless">
+        <div class="card-img">
+          <img src="assets/benefits/Massage.jpg" alt="Massage Image" />
+        </div>
+        <div class="card-content">
+          <h4>Relieve Tension in Sore Muscle</h4>
+          <div class="card-content_description">
+            The ultrasonic bubble massages 320 main acupunture point across the body. 15 minutes bath is equal to an hour professional massage.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-container">
+      <div class="card shadowless">
+        <div class="card-img benefit-detox">
+          <img src="assets/benefits/Deep-Cleansing.jpg" alt="Detoxification Image" />
+        </div>
+        <div class="card-content">
+          <h4>Skin conditioning and detoxification</h4>
+          <div class="card-content_description">
+            It cleans 1.5 million capillaries in the skin, which bring nourishment and allowing the skin tissues to breathe better.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-container">
+      <div class="card shadowless">
+      <div class="card-img benefit-slim">
+        <img src="assets/benefits/Slimming.jpg" alt="Slimming Image" />
+      </div>
+      <div class="card-content">
+        <h4>Stimulate exercise and contouring</h4>
+        <div class="card-content_description">
+          The ultrasonic bubble stimulates your body as if you are running 3 -5 KM with just 10 minutes of bathing.
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="section-footer">
+    <a href="#">Learn More about Hydrotherapy Spa</a>
+</section>
+
+<section class="section services">
+  <div class="section-title">
+    <h3>Services</h3>
+  </div>
+  <div class="cards grid-1120">
+    <div class="card-container">
+      <a href="#">
+        <div class="card">
+        <div class="card-img">
+          <img src="assets/services/indulgence.jpg" alt="Complete Body Indulgence Image" />
+        </div>
+        <div class="card-content">
+          <h4>Complete Body Indulgence</h4>
+          <div class="card-content_description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt libero eget metus venenatis, at eleifend libero fermentum. Suspendisse eros massa, ullamcorper faucibus lectus vitae.
+          </div>
+          <div class="card-content_link">
+            <a href="#">See Complete Body Indulgence Services</a>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+    <div class="card-container">
+      <a href="#">
+        <div class="card">
+        <div class="card-img"><img src="assets/services/classical-massage.jpg" alt="Relieve sore and muscle tension image" /></div>
+        <div class="card-content">
+          <h4>Classical Body Massage</h4>
+          <div class="card-content_description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt libero eget metus venenatis, at eleifend libero fermentum. Suspendisse eros massa, ullamcorper faucibus lectus vitae.
+          </div>
+          <div class="card-content_link">
+            <a href="#">See Classical Body Massage Services</a>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+    <div class="card-container">
+      <a href="#">
+        <div class="card">
+        <div class="card-img"><img src="assets/services/foot.jpg" alt="Relieve sore and muscle tension image" /></div>
+        <div class="card-content">
+          <h4>Foot</h4>
+          <div class="card-content_description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt libero eget metus venenatis, at eleifend libero fermentum. Suspendisse eros massa, ullamcorper faucibus lectus vitae.
+          </div>
+          <div class="card-content_link">
+            <a href="#">See Foot Services</a>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+    <div class="card-container">
+      <a href="#">
+        <div class="card">
+        <div class="card-img"><img <img src="assets/services/facial.jpg" alt="Relieve sore and muscle tension image" /></div>
+        <div class="card-content">
+          <h4>Facial</h4>
+          <div class="card-content_description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt libero eget metus venenatis, at eleifend libero fermentum. Suspendisse eros massa, ullamcorper faucibus lectus vitae.
+          </div>
+          <div class="card-content_link">
+            <a href="#">See All Facial Services</a>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+  </div>
+  <div class="section-footer">
+    <a href="#">See All Services</a>
+</section>
+
+<!-- <section class="section testimonials">
+  <div class="section-title">
+    <h3>What People Say About Us</h3>
+  </div>
+  <div uk-slideshow="max-height:600px">
+    <ul class="uk-slideshow-items">
+        <li>
+          <img src="assets/logo.png" />
+          <div class="uk-position-center uk-position-medium uk-text-center">
+            <div class="uk-position-center uk-position-small uk-text-center uk-light">
+                <h2>Heading</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div> -->
+        </li>
+    </ul>
+  </div>
+</section>
+
+<section class="section booknow">
+  <div class="grid-1120">
+    <div class="booknow-message">
+      <h2>Ready to relax and getaway?</h2>
+      <!-- <h2>Book online</h2> -->
+    </div>
+    <div class="booknow-buttons">
+      <button class="btn btn-inverse">Book Now</button>
+      <!-- <button class="btn btn-inverse">Contact Us</button> -->
+    </div>
+  </div>
+</section>
+<?php  echo file_get_contents("html/footer.html"); ?>
