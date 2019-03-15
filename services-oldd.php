4d78cc9770d8c32505d18a14ccc86a362ee7eebd
Micheal Seng <michealseng@crosshatch.tv>
2019-03-14 23:17:26 -0700

    Updated logo, favicon, and services.

---
+++ services-oldd.php
@@ -0,0 +1,391 @@
+<?php  echo file_get_contents("html/header.html"); ?>
+<?php  echo file_get_contents("html/top-nav.html"); ?>
+    <div class="content">
+      <ul class="uk-subnav services-nav">
+        <li class="uk-active"><a href="#signature-rituals" class="services-nav-items">Signature Rituals</a></li>
+        <li><a href="#combinations" class="services-nav-items">Combinations</a></li>
+        <li><a href="#massage" class="services-nav-items">Massages</a></li>
+        <li><a href="#foot" class="services-nav-items">Foot Treatments</a></li>
+        <li><a href="#bath" class="services-nav-items">Ultrasonic Bubble Bath</a></li>
+        <li><a href="#face" class="services-nav-items">Facial Treatments</a></li>
+      </ul>
+      <div class="alacart-container">
+        <div class="service">
+          <a class="anchor" id="signature-rituals">&nbsp;</a>
+          <h3 class="service-category">Signature Rituals</h3>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Ultimate De-stress</h4>
+              <div class="service-pricing">
+                <span>3 hrs | Rp.880k</span>
+              </div>
+            </div>
+            <span>Include:</span>
+            <ul>
+              <li>Peppermint Sea Salt Foot Bath</li>
+              <li>Warm Water Body Cleanse</li>
+              <li>Basil Mint Body Scrub</li>
+              <li>Lavender Hydrobath</li>
+              <li>Balinese Massage</li>
+              <li>Cold Stone Face Massage</li>
+            </ul>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Royal Experience</h4>
+              <div class="service-pricing">
+                <span>3 hrs | Rp.880k</span>
+              </div>
+            </div>
+            <span>Include:</span>
+            <ul>
+              <li>Lavender Sea Salt Foot Bath</li>
+              <li>Warm Water Body Cleanse</li>
+              <li>Honey Body Scrub</li>
+              <li>Lavender & Eucalyptus Hydrobath</li>
+              <li>Aromatic Full-body Massage</li>
+              <li>Face Lift Massage</li>
+            </ul>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Rose Heaven</h4>
+              <div class="service-pricing">
+                <span>3 hrs 30 min | Rp.980k</span>
+              </div>
+            </div>
+            <span>Include:</span>
+            <ul>
+              <li>Rose Sea Salt Foot Bath</li>
+              <li>Warm Water Body Cleanse</li>
+              <li>Rose Body Scrub</li>
+              <li>Rose Clay Mask </li>
+              <li>Rose Pamper Hydrobath</li>
+              <li>Aromatic Full-body Massage</li>
+              <li>Cold Stone Face Massage</li>
+            </ul>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Aloe Vera Skin Replenish</h4>
+              <div class="service-pricing">
+                <span>3 hrs 30 min | Rp.980k</span>
+              </div>
+            </div>
+            <span>Include:</span>
+            <ul>
+              <li>Peppermint Sea Salt Foot Bath</li>
+              <li>Warm Water Body Cleanse</li>
+              <li>Aloe Vera Body Scrub</li>
+              <li>Aloe Vera Body Mask</li>
+              <li>Aloe Vera Hair Mask</li>
+              <li>Camomile Hydrobath</li>
+              <li>Aromatic Full-body Massage</li>
+            </ul>
+          </div>
+        </div>
+        <div class="service">
+          <a class="anchor" id="combinations">&nbsp;</a>
+          <h3 class="service-category">Combinations</h3>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Quick Spa</h4>
+              <div class="service-pricing">
+                <span>2 hrs | Rp.480k</span>
+              </div>
+            </div>
+            <span>Improve your skin. Look fantastic. Feel incredible. We invite you to relax and rejuvenate your body, mind, and spirit in a short time possible.<br><br>
+            Include:
+            </span>
+            <ul>
+              <li>Foot Bath</li>
+              <li>Body Scrub</li>
+              <li>Ultrasonic Bubble Bath</li>
+              <li>Aromatic Body Massage</li>
+            </ul>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Oriental Express Relieves</h4>
+              <div class="service-pricing">
+                <span>2 hours | Rp.380k</span>
+              </div>
+            </div>
+            <span>Refresh yourself with our in-house remedy for city-gateaway treatment that combines 60 mins Balinese  Massage with 60 mins Reflexology.		</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Top to Toe</h4>
+              <div class="service-pricing">
+                <span>1 hr | Rp.280k</span>
+              </div>
+            </div>
+            <span>Our signature therapy, a combination of Oriental Foot massage with head, shoulder and arm massage by two therapists at the same time guarantees you will leave our Retreat feeling as light as a feather.</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Happy Weekend Getaway</h4>
+              <div class="service-pricing">
+                <span>2 hrs 30 mins | Rp.580k</span>
+              </div>
+            </div>
+            <span>Pamper yourself with this treatment starting with Ultrasonic Bubble Bath, 90 mins Balinese Massage and 30 mins Reflexology.</span>
+          </div>
+        </div>
+        <div class="service">
+          <a class="anchor" id="massage">&nbsp;</a>
+          <h3 class="service-category">Massages</h3>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Aromatic</h4>
+              <div class="service-pricing">
+                <span>60 mins | Rp.200k</span>
+                <span>90 mins | Rp.280k</span>
+                <span>120 mins | Rp.340k</span>
+              </div>
+            </div>
+            <span>A Javanese massage complemented with your choise of an essential oils to improve your well being to restore balance, harmony and a relaxed mind.</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Radiant</h4>
+              <div class="service-pricing">
+                <span>90 mins | Rp.340k</span>
+                <span>120 mins | Rp.420k</span>
+              </div>
+            </div>
+            <span>Our signature massage using Body Scrub to relax and give radiance to your skin. </span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Balinese</h4>
+              <div class="service-pricing">
+                <span>60 mins | Rp.220k</span>
+                <span>90 mins | Rp.300k</span>
+                <span>120 mins | Rp.360k</span>
+              </div>
+            </div>
+            <span>This energizing massage uses a combinaion of thumb and palm pressure with deep strokes and is perfect for those who prefer a deeper pressure.		</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Bali Volcano Hot Stone</h4>
+              <div class="service-pricing">
+                <span>90 min | Rp.340k</span>
+                <span>120 min | Rp.420k</span>
+              </div>
+            </div>
+            <span>A Hot Stone massage treatment, uses water heated stone, to promote deep muscle and tissue relaxation, improve blood circulation and calm the psyche.		</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Herbal Compress</h4>
+              <div class="service-pricing">
+                <span>90 mins | Rp.340k</span>
+                <span>120 mins | Rp.420k</span>
+              </div>
+            </div>
+            <span>This massage uses organic herbal ingredients that is heated to soothe and relax muscles.</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Customized Deep Tissue</h4>
+              <div class="service-pricing">
+                <span>60 mins | Rp.220k</span>
+                <span>90 mins | Rp.300k</span>
+                <span>120 mins | Rp.360k</span>
+              </div>
+            </div>
+            <span>This muscle make-over uses deep pressure to target stubborn knots in areas of chronic stress, releasing sluggish blood flow and easing tensin from top to toe.		</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Four Hand Massage</h4>
+              <div class="service-pricing">
+                <span>90 mins | Rp.340k</span>
+                <span>120 mins | Rp.420k</span>
+              </div>
+            </div>
+            <span>This massage is handled by two therapists. </span>
+          </div>
+
+        </div>
+        <div class="service">
+          <a class="anchor" id="foot">&nbsp;</a>
+          <h3 class="service-category">Foot Treatments</h3>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Ultrasonic Foot Bubble Bath</h4>
+              <div class="service-pricing">
+                <span>30 min | Rp.200k</span>
+              </div>
+            </div>
+
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Foot Scrub</h4>
+              <div class="service-pricing">
+                <span>20 min | Rp.80k</span>
+              </div>
+            </div>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Reflexology</h4>
+              <div class="service-pricing">
+                <span>60 mins | Rp.160k</span>
+                <span>90 mins | Rp.240k</span>
+                <span>120 mins | Rp.300k</span>
+              </div>
+            </div>
+            <span>Relieves headache and migraine, enhances the immune system, improves blood circulation, normalizes organ and gland function – and feels wonderful!</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Foot Spa</h4>
+              <div class="service-pricing">
+                <span>90 mins | Rp.220k</span>
+              </div>
+            </div>
+            <span>Include:</span>
+            <ul>
+              <li>Peppermint Sea Salt Foot Bath</li>
+              <li>Foot Scrub</li>
+              <li>Reflexology</li>
+              <li>Foot Moisturizer</li>
+              <li>Neck, Shoulder and Hand Massage </li>
+            </ul>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Naya Special Foot Spa</h4>
+              <div class="service-pricing">
+                <span>120 mins | Rp.380k</span>
+              </div>
+            </div>
+            <span>Include:</span>
+            <ul>
+              <li>Peppermint Sea Salt Foot Bath</li>
+              <li>Foot Scrub</li>
+              <li>Ultrasonic Foot Bubble Bath</li>
+              <li>Reflexology</li>
+              <li>Foot Moisturizer</li>
+              <li>Neck, Shoulder and Hand Massage </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="service">
+          <a class="anchor" id="bath">&nbsp;</a>
+          <h3 class="service-category">Ultrasonic Bubble Bath</h3>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Ultrasonic Body Bubble Bath</h4>
+              <div class="service-pricing">
+                <span>20 mins | Rp.200k</span>
+              </div>
+            </div>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Ultrasonic Foot Bubble Bath</h4>
+              <div class="service-pricing">
+                <span>35 mins | Rp.220k</span>
+              </div>
+            </div>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Lavender Deep Sleep Bath</h4>
+              <div class="service-pricing">
+                <span>20 mins | Rp.250k</span>
+              </div>
+            </div>
+            <span>This is the best remedy to get deep sleep night. </span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Rose Royal Bath</h4>
+              <div class="service-pricing">
+                <span>20 mins | Rp.250k</span>
+              </div>
+            </div>
+            <span>Pamper yourself with this romantic rose petal bath and our signature bath oil. </span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Muscle and Nerve Relieve Bath</h4>
+              <div class="service-pricing">
+                <span>20 mins | Rp.250k</span>
+              </div>
+            </div>
+            <span>Customized water-soluable bath oil to release your muscle tension and pain.</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Cold and Cough Bath</h4>
+              <div class="service-pricing">
+                <span>20 mins | Rp.250k</span>
+              </div>
+            </div>
+            <span>Relieve all the blockages of your sinuses, heal you in no time</span>
+          </div>
+        </div>
+
+
+        <div class="service">
+          <a class="anchor" id="face">&nbsp;</a>
+          <h3 class="service-category">Facial Treatments</h3>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Face Accupressure</h4>
+              <div class="service-pricing">
+                <span>30 mins | Rp.120k</span>
+                <span>60 mins | Rp.200k</span>
+              </div>
+            </div>
+            <span>Pamper your face and relax the tension in your face muscles.</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Ear Candle</h4>
+              <div class="service-pricing">
+                <span>30 min | Rp.120k</span>
+              </div>
+            </div>
+            <small>Beneficial to alleviate sinusitis, insomnia, headache & migraine.</small>
+          </div>
+
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Eye Compress</h4>
+              <div class="service-pricing">
+                <span>30 mins | Rp.150k</span>
+              </div>
+            </div>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Refreshing Facial</h4>
+              <div class="service-pricing">
+                <span>90 mins | Rp.380k</span>
+              </div>
+            </div>
+            <span>This is the ultimate face cleansing with the combination of ultrasonic bubble bath that includes make-up cleansing, scrub, blackheads cleansing, face mask, tonic and cream application.</span>
+          </div>
+          <div class="service-row">
+            <div class="service-title">
+              <h4>Anti-Aging Facial</h4>
+              <div class="service-pricing">
+                <span>90 mins | Rp.480k</span>
+              </div>
+            </div>
+            <span>Beyong facial cleansing, this aims for cell generation,to  rejuvenates, moisturizes and improve skin texture.</span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+<?php  echo file_get_contents("html/footer.html"); ?>
